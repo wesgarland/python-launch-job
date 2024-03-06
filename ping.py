56373e5d9e87a52196ebfbff4be43160102d89ce
Wes Garland <wes@distributive.network>
2024-03-06 15:09:49 +0000

    ping.py - register event handlers on connection instance

--- ping.py
+++ ping.py
@@ -13,8 +13,19 @@ import asyncio
 async def go():
     dcpConfig = pm.globalThis.dcpConfig
     Connection = pm.globalThis.dcp.protocol.Connection
-    target = (dcpConfig.scheduler.services.jobSubmit)
+    target = dcpConfig.scheduler.services.jobSubmit
     conn = pm.new(Connection)(target)
+
+    conn.on("error",              lambda error:    print("connection error:", error))
+    conn.on("ready",              lambda:          print("connection ready"))
+    conn.on("session",            lambda unused:   print("session established", conn.dcpsid))
+    conn.on("end",                lambda:          print("connection closing"))
+    conn.on("close",              lambda:          print("connection closed"))
+    conn.on("disconnect",         lambda location: print("transport layer disconnected from", location.href))
+    conn.on("connect",            lambda location: print("transport layer disconnected to", location.href))
+    conn.on("connectionProgress", lambda obj:      print("connection progress", obj))
+    conn.on("beforeSend",         lambda message:  print("sending message", message))
+
     conn.state.on("change", lambda state, unused: print("connection to", target.location.href, "now", state))
     conn.connect()
     await conn.keepalive()
